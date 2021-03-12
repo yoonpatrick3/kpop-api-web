a9b60710d09b7ed30f9d8d9e74f81948080305ff
Patrick Yoon <yoonpatrick3@gmail.com>
2021-03-11 23:40:11 -0500

    made code better lol

--- app.py
+++ app.py
@@ -4,27 +4,31 @@ from kpopy import get_specific_artist, get_similar_artist
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+def parse_data(data):
+    stage_name = data.get('stage_name', '')
+    full_name = data.get('full_name', '')
+    korean_name = data.get('korean_name', '')
+    korean_stage_name = data.get('korean_stage_name', '')
+    dob = data.get('date_of_birth', '')
+    group = data.get('group', '')
+    country = data.get('country', '')
+    second_country = data.get('second_country', '')
+    height = data.get('height', '')
+    weight = data.get('weight', '')
+    birthplace = data.get('birthplace', '')
+    other_group = data.get('other_group', '')
+    former_group = data.get('former_group', '')
+    gender = data.get('gender', '')
+    position = data.get('position', '')
+    instagram = data.get('instagram', '')
+    twitter = data.get('twitter', '')
+    return stage_name, full_name, korean_name, korean_stage_name, dob, group, country, second_country, height, weight, birthplace, other_group, former_group, gender, position, instagram, twitter
+
 #/equal?
 @app.route("/equals", methods=['GET'])
 def equal():
     data = request.args
-    stage_name = data.get('stage_name') if data.get('stage_name') else ''
-    full_name = data.get('full_name') if data.get('full_name') else ''
-    korean_name = data.get('korean_name') if data.get('korean_name') else ''
-    korean_stage_name = data.get('korean_stage_name') if data.get('korean_stage_name') else ''
-    dob = data.get('date_of_birth') if data.get('date_of_birth') else ''
-    group = data.get('group') if data.get('group') else ''
-    country = data.get('country') if data.get('country') else ''
-    second_country = data.get('second_country') if data.get('second_country') else ''
-    height = data.get('height') if data.get('height') else ''
-    weight = data.get('weight') if data.get('weight') else ''
-    birthplace = data.get('birthplace') if data.get('birthplace') else ''
-    other_group = data.get('other_group') if data.get('other_group') else ''
-    former_group = data.get('former_group') if data.get('former_group') else ''
-    gender = data.get('gender') if data.get('gender') else ''
-    position = data.get('position') if data.get('position') else ''
-    instagram = data.get('instagram') if data.get('instagram') else ''
-    twitter = data.get('twitter') if data.get('twitter') else ''
+    stage_name, full_name, korean_name, korean_stage_name, dob, group, country, second_country, height, weight, birthplace, other_group, former_group, gender, position, instagram, twitter = parse_data(data)
     print("getting:", stage_name, full_name, korean_name, korean_stage_name)
     kpop_data = get_specific_artist(stage_name = stage_name, full_name = full_name, korean_name = korean_name,
                                 korean_stage_name = korean_stage_name, dob = dob, group = group, country = country,
@@ -38,23 +42,7 @@ def equal():
 @app.route("/contains", methods=['GET'])
 def contains():
     data = request.args
-    stage_name = data.get('stage_name') if data.get('stage_name') else ''
-    full_name = data.get('full_name') if data.get('full_name') else ''
-    korean_name = data.get('korean_name') if data.get('korean_name') else ''
-    korean_stage_name = data.get('korean_stage_name') if data.get('korean_stage_name') else ''
-    dob = data.get('date_of_birth') if data.get('date_of_birth') else ''
-    group = data.get('group') if data.get('group') else ''
-    country = data.get('country') if data.get('country') else ''
-    second_country = data.get('second_country') if data.get('second_country') else ''
-    height = data.get('height') if data.get('height') else ''
-    weight = data.get('weight') if data.get('weight') else ''
-    birthplace = data.get('birthplace') if data.get('birthplace') else ''
-    other_group = data.get('other_group') if data.get('other_group') else ''
-    former_group = data.get('former_group') if data.get('former_group') else ''
-    gender = data.get('gender') if data.get('gender') else ''
-    position = data.get('position') if data.get('position') else ''
-    instagram = data.get('instagram') if data.get('instagram') else ''
-    twitter = data.get('twitter') if data.get('twitter') else ''
+    stage_name, full_name, korean_name, korean_stage_name, dob, group, country, second_country, height, weight, birthplace, other_group, former_group, gender, position, instagram, twitter = parse_data(data)
     print("getting:", stage_name, full_name, korean_name, korean_stage_name, dob, group, country, second_country, height, weight,
                      birthplace, other_group, former_group, gender, position, instagram, twitter)
     kpop_data = get_similar_artist(stage_name = stage_name, full_name = full_name, korean_name = korean_name,
